7632566c523051f588a0cb06ecba1cf5f9d3a29f
Anthony Cordero <anthonycordehu@gmail.com>
2025-01-12 22:43:34 -0500

    automatic carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,19 +2,19 @@ import { Carousel } from "@material-tailwind/react";
  
 export function CarouselDefault() {
   return (
-    <Carousel className="rounded-xl">
+    <Carousel className="rounded-xl" autoplay={true} loop={true}>
       <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        src="public\img\carousel-img1.jpg"
         alt="image 1"
         className="h-full w-full object-cover"
       />
       <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        src="public\img\carousel-img2.jpg"
         alt="image 2"
         className="h-full w-full object-cover"
       />
       <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        src="public\img\carousel-img3.jpg"
         alt="image 3"
         className="h-full w-full object-cover"
       />
