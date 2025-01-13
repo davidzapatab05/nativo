40169bb664d08ec6177098fbfeb5251c72ee88ec
davidzapatab05 <davidzapata.dz051099@gmail.com>
2025-01-12 23:54:11 -0500

    Commit
    Commit

--- src/data/features-data.js
+++ src/data/features-data.js
@@ -1,29 +1,32 @@
 import {
-  ChatBubbleBottomCenterTextIcon,
-} from "@heroicons/react/24/solid";
+  InformationCircleIcon,
+  BriefcaseIcon,
+  ShieldCheckIcon,
+} from "@heroicons/react/24/outline";
 
 export const featuresData = [
   {
     color: "gray",
-    title: "Awarded Agency",
-    icon: ChatBubbleBottomCenterTextIcon,
+    title: "Sobre nosotros",
+    icon: InformationCircleIcon,
     description:
-      "Divide details about your product or agency work into parts. A paragraph describing a feature will be enough.",
+      "CONSTRUCTORA NATIVO S.A.C a lo largo de los años ha adquirido el conocimiento y la experiencia necesaria para ofrecer a los clientes un servicio de calidad, y desempeñar las labores encomendadas, relacionadas con la ejecución de proyectos inmobiliarios en Piura y la elaboración de expedientes técnicos.",
   },
   {
     color: "gray",
-    title: "Free Revisions",
-    icon: ChatBubbleBottomCenterTextIcon,
+    title: "Lo que hacemos",
+    icon: BriefcaseIcon,
     description:
-      "Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious.",
+      "Somos una empresa constructora en Piura especializada en prestar servicios integrales para la ejecución de proyectos inmobiliarios en Piura. Entre nuestros servicios se encuentran: ejecución de obras de ingeniería civil, demoliciones, alquiler de maquinaria pesada, o la elaboración y ejecución de proyectos de construcción.",
   },
   {
     color: "gray",
-    title: "Verified Company",
-    icon: ChatBubbleBottomCenterTextIcon,
+    title: "Nuestra responsabilidad",
+    icon: ShieldCheckIcon,
     description:
-      "Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!",
+      "CONSTRUCTORA NATIVO S.A.C es mucho más que una simple empresa constructora. Desde el principio, contribuimos y nos implicamos con el desarrollo social del Perú, así como respetamos tanto la normativa referente a los temas de seguridad ocupacional como el medio ambiente.",
   },
 ];
+
 
 export default featuresData;
